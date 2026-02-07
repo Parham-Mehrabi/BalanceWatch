edd9b1c5015ec345895384a5f90fd18a5079e9b3
parham <mehrabipourparham@gmail.com>
2026-02-07 08:21:50 +0330

    add account app and create a costume user model
    the new user model is basically identical to the old one but i defined another class for it just in case i ever wanted to expand it in future

--- balance_watch/settings.py
+++ balance_watch/settings.py
@@ -35,6 +35,9 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+
+    "account",
+
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
@@ -119,3 +122,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = "static/"
+
+
+
+# --
+
+AUTH_USER_MODEL = "account.User"
