5a2e7e163263e2a8b4c6be69e5d35e71e1a64be5
parham <mehrabipourparham@gmail.com>
2026-02-12 06:51:38 +0330

    create Subscription model and update account.admin

--- account/admin.py
+++ account/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from django.contrib.auth import get_user_model
+from account.models import Subscription
+
+UserModel = get_user_model()
 
 # Register your models here.
+
+
+admin.site.register(UserModel)
+admin.site.register(Subscription)
--- account/models.py
+++ account/models.py
@@ -1,7 +1,22 @@
 from django.contrib.auth.models import AbstractUser
-
+from django.db import models
+from django.utils import timezone
+from django.conf import settings
 
 
 class User(AbstractUser):
     
     pass    # default user would do for now
+
+
+
+class Subscription(models.Model):
+
+    user = models.OneToOneField(to=settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="subscription")
+    ends_at = models.DateTimeField(default=timezone.now)
+    
+    @property
+    def is_active(self) -> bool:
+        if self.ends_at:
+            return self.ends_at > timezone.now()
+        return True
