8021b526c73f8b9e50113ccf467e6588f95400a3
parham <parham.mehrabi.webdev@gmail.com>
2026-02-22 16:33:18 +0330

    fix missed space in caddy config and add --noinput for collectstatic

--- django-app/bootstrap-deploy.sh
+++ django-app/bootstrap-deploy.sh
@@ -1,6 +1,6 @@
 python3 manage.py makemigrations
 python3 manage.py migrate
-python3 manage.py collectstatic
+python3 manage.py collectstatic --noinput
 gunicorn balance_watch.wsgi --bind 0.0.0.0:8000 --access-logfile - --error-logfile - --workers 3 --threads 2
 
 
